d717cdbc57e93c7655619f6f6ee1f683a5b5e944
Mohammad Abdullah <110193381+MohammadAbdullah7972@users.noreply.github.com>
2024-07-16 01:25:50 +0530

    Update Horsebooking.jsx

--- src/pages/Horsebooking.jsx
+++ src/pages/Horsebooking.jsx
@@ -2,11 +2,13 @@
 import React from 'react';
 import Bradcrumb from './Horsebooking/Bradcrumb';
 import RideBooking from './Horsebooking/RideBooking';
+import Footer from '../components/Footer';
 const HorseBooking = () => {
     return (
-        <div className="relative -z-10 mt-[-80px]">
-            <Bradcrumb />
+        <div className="relative">
+            <Bradcrumb className=""/>
             <RideBooking/>
+            <Footer/>
         </div>
     );
 };
